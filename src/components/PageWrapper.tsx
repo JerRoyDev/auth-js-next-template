0741e8d7b68ea8604f8d424540c39e822d0051a3
JerRoyDev <jerryroyolsson@gmail.com>
2025-11-11 10:51:23 +0100

    feat: update PageWrapper to use flex-col for vertical alignment

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -10,7 +10,10 @@ interface PageWrapperProps {
 const PageWrapper = ({ children, className }: PageWrapperProps) => {
   return (
     <div
-      className={cn('flex items-center justify-center px-2 py-2', className)}
+      className={cn(
+        'flex flex-col items-center justify-center px-2 py-2',
+        className
+      )}
     >
       {children}
     </div>
