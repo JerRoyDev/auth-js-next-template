69ff35c8b240b9a9ff399b83931dfc7e5607c76d
JerRoyDev <jerryroyolsson@gmail.com>
2025-11-23 01:16:30 +0100

    feat: add ModeToggle component to AppHeader for theme switching

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -12,6 +12,7 @@ import { Separator } from '@/components/ui/separator';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { usePathname } from 'next/navigation';
 import { useMemo } from 'react';
+import ModeToggle from '@/components/mode-toggle';
 
 export function AppHeader() {
   const pathname = usePathname();
@@ -26,28 +27,31 @@ export function AppHeader() {
   }, [pathname]);
 
   return (
-    <header className='flex h-16 fixed bg-sidebar border-b w-full border-border shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
-      <div className='flex items-center gap-2 px-4'>
-        <SidebarTrigger className='-ml-1' />
-        <Separator orientation='vertical' className='mr-2 h-4' />
-        <Breadcrumb>
-          <BreadcrumbList>
-            {breadcrumbs.map((crumb, index) => (
-              <div key={crumb.href} className='flex items-center gap-2'>
-                <BreadcrumbItem>
-                  {crumb.isLast ? (
-                    <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
-                  ) : (
-                    <BreadcrumbLink href={crumb.href}>
-                      {crumb.label}
-                    </BreadcrumbLink>
-                  )}
-                </BreadcrumbItem>
-                {!crumb.isLast && <BreadcrumbSeparator />}
-              </div>
-            ))}
-          </BreadcrumbList>
-        </Breadcrumb>
+    <header className='flex h-16 sticky top-0 z-10 bg-sidebar border-b border-border shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+      <div className='flex items-center justify-between gap-2 px-4 '>
+        <div className='flex items-center gap-2'>
+          <SidebarTrigger className='-ml-1' />
+          <Separator orientation='vertical' className='mr-2 h-4' />
+          <Breadcrumb>
+            <BreadcrumbList>
+              {breadcrumbs.map((crumb, index) => (
+                <div key={crumb.href} className='flex items-center gap-2'>
+                  <BreadcrumbItem>
+                    {crumb.isLast ? (
+                      <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
+                    ) : (
+                      <BreadcrumbLink href={crumb.href}>
+                        {crumb.label}
+                      </BreadcrumbLink>
+                    )}
+                  </BreadcrumbItem>
+                  {!crumb.isLast && <BreadcrumbSeparator />}
+                </div>
+              ))}
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+        <ModeToggle />
       </div>
     </header>
   );
