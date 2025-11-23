93ce7ab55530d33538cfb2fd59230702a32654cc
JerRoyDev <jerryroyolsson@gmail.com>
2025-11-23 01:19:08 +0100

    feat: wrap ContentLayout in FadeInWrapper for enhanced transition effects

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -3,6 +3,7 @@ import { AppSidebar } from '@/components/sidebar/app-sidebar';
 import { AppHeader } from '@/components/app/AppHeader';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import ContentLayout from '@/components/layouts/ContentLayout';
+import FadeInWrapper from '@/components/FadeInWrapper';
 
 const AppLayout = async ({ children }: { children: React.ReactNode }) => {
   await requireAuth();
@@ -12,9 +13,11 @@ const AppLayout = async ({ children }: { children: React.ReactNode }) => {
       <AppSidebar />
       <SidebarInset>
         <AppHeader />
-        <ContentLayout maxWidth='screen-2xl' noTopMargin>
-          {children}
-        </ContentLayout>
+        <FadeInWrapper>
+          <ContentLayout maxWidth='screen-2xl' noTopMargin>
+            {children}
+          </ContentLayout>
+        </FadeInWrapper>
       </SidebarInset>
     </SidebarProvider>
   );
