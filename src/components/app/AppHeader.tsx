823190c51a172880600d79800cd5d2b399579b24
JerRoyDev <jerryroyolsson@gmail.com>
2025-11-23 01:19:12 +0100

    fix: add full width to AppHeader container for better layout consistency

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -28,7 +28,7 @@ export function AppHeader() {
 
   return (
     <header className='flex h-16 sticky top-0 z-10 bg-sidebar border-b border-border shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
-      <div className='flex items-center justify-between gap-2 px-4 '>
+      <div className='flex items-center justify-between gap-2 px-4 w-full '>
         <div className='flex items-center gap-2'>
           <SidebarTrigger className='-ml-1' />
           <Separator orientation='vertical' className='mr-2 h-4' />
